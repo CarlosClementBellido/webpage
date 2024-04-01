9dc7725b7eb21e605ebe9bcc61f9b7f4c1afa998
Carlos Clement Bellido <clementbellido@google.com>
2024-04-01 14:23:54 +0200

    Added lists as templates with comments on hover

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,11 @@ import cv from '../cv.json' assert { type: 'json' };
 window.onload = () => {
     replaceSingleStrings();
     createTemplates();
-
     startIgAnimation();
+    createListFunctionalities();
 };
+
+//#region Ig animation
 
 let posts;
 let currentIndex = 0;
@@ -25,8 +27,13 @@ function showNextPost() {
 function startIgAnimation() {
     posts = document.querySelectorAll('.t.post');
     showNextPost();
+    showNextPost();
     setInterval(showNextPost, 10000);
 }
+
+//#endregion Ig animation
+
+//#region Template creation and string substitution
 
 function createTemplates() {
     const templates = document.getElementsByClassName("t");
@@ -64,3 +71,38 @@ function getMatches(element) {
 
     return matches;
 }
+
+//#endregion Template creation and string substitution
+
+//#region List functionalities creation
+
+function createListFunctionalities() {
+    const commentsHover = document.querySelectorAll(".comment-hover");
+
+    Array.from(commentsHover).forEach(commentHover => {
+        const comment = commentHover.querySelector(".comment");
+        const closeButton = comment.querySelector(".close-comment");
+
+
+        closeButton.addEventListener("click", () => {
+            comment.classList.remove("active");
+            closeButton.classList.remove("active");            
+        });
+
+        commentHover.addEventListener("click", (event) => {
+            if (event.target.classList.contains("comment-hover")) {
+                const activeComments = document.querySelectorAll(".comment.active");
+                Array.from(activeComments).forEach(activeComment => {
+                    activeComment.classList.remove("active");
+                    activeComment.querySelector(".close-comment").classList.remove("active");
+                });
+
+                comment.classList.add("active");
+                closeButton.classList.add("active");
+            }
+        });
+
+    });
+}
+
+//#endregion List functionalities creation
